29da226ae5e75bfd8ca21e2028047ad90dac8da5
cinquecentoandrey <chehonte16@gmail.com>
2022-09-06 20:53:48 +0300

    Added the ability to see all users and add an event

--- src/main/java/com/cinquecento/sporteventmanagementsystem/controller/AdminController.java
+++ src/main/java/com/cinquecento/sporteventmanagementsystem/controller/AdminController.java
@@ -1,16 +1,67 @@
 package com.cinquecento.sporteventmanagementsystem.controller;
 
+import com.cinquecento.sporteventmanagementsystem.model.Event;
+import com.cinquecento.sporteventmanagementsystem.model.Participant;
+import com.cinquecento.sporteventmanagementsystem.service.EventsService;
+import com.cinquecento.sporteventmanagementsystem.service.ParticipantsService;
+import com.cinquecento.sporteventmanagementsystem.util.validators.EventValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/admin")
 public class AdminController {
+
+    private final EventsService eventsService;
+    private final ParticipantsService participantsService;
+
+    private final EventValidator eventValidator;
+
+    @Autowired
+    public AdminController(EventsService eventsService, ParticipantsService participantsService, EventValidator eventValidator) {
+        this.eventsService = eventsService;
+        this.participantsService = participantsService;
+        this.eventValidator = eventValidator;
+    }
 
     @GetMapping()
     public String adminPage() {return "admin/adminPage"; }
 
-    @GetMapping("/addNewEvent")
-    public String addNewEvent() {return "admin/addNewEvent"; }
+    @GetMapping("/eventRegistration")
+    public String eventRegistration(@ModelAttribute("event") Event event) { return "admin/eventRegistration"; }
+
+    @PostMapping("/eventRegistration")
+    public String performEventRegistration(@ModelAttribute("event") @Valid Event event,
+                                           BindingResult bindingResult) {
+
+        eventValidator.validate(event, bindingResult);
+        if(bindingResult.hasErrors()) {
+            return "admin/eventRegistration";
+        }
+
+        eventsService.save(event);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/showAllParticipants")
+    public String showAllParticipants(Model model) {
+        List<Participant> participants = participantsService.findAll();
+
+        participants = participants.stream().filter(participant ->
+            participant.getRole().equals("ROLE_USER")).collect(Collectors.toList());
+
+        model.addAttribute("participants", participants);
+        return "admin/participants";
+    }
+
+
 }
