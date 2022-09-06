749704949cd5e9d2a1ef31c114ce06f33c02000e
cinquecentoandrey <chehonte16@gmail.com>
2022-09-06 20:55:47 +0300

    Added custom methods

--- src/main/java/com/cinquecento/sporteventmanagementsystem/repository/EventsRepository.java
+++ src/main/java/com/cinquecento/sporteventmanagementsystem/repository/EventsRepository.java
@@ -4,6 +4,10 @@ import com.cinquecento.sporteventmanagementsystem.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EventsRepository extends JpaRepository<Event, Integer> {
+    Optional<Event> findByEventName(String eventName);
+
 }
--- src/main/java/com/cinquecento/sporteventmanagementsystem/repository/ParticipantsRepository.java
+++ src/main/java/com/cinquecento/sporteventmanagementsystem/repository/ParticipantsRepository.java
@@ -4,6 +4,10 @@ import com.cinquecento.sporteventmanagementsystem.model.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParticipantsRepository extends JpaRepository<Participant, Integer> {
+    Optional<Participant> findByUsername(String username);
+
 }
