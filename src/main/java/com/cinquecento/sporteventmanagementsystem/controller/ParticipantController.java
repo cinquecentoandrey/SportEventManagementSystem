65b6fa4aea81f758f5fbf0498a12743e98e9c8b7
cinquecentoandrey <chehonte16@gmail.com>
2022-09-06 18:50:55 +0300

    Updated ParticipantController.java, added the ability to see the data about the user

--- src/main/java/com/cinquecento/sporteventmanagementsystem/controller/ParticipantController.java
+++ src/main/java/com/cinquecento/sporteventmanagementsystem/controller/ParticipantController.java
@@ -1,14 +1,41 @@
 package com.cinquecento.sporteventmanagementsystem.controller;
 
+import com.cinquecento.sporteventmanagementsystem.model.Participant;
+import com.cinquecento.sporteventmanagementsystem.security.ParticipantsDetails;
+import com.cinquecento.sporteventmanagementsystem.service.ParticipantsService;
+import com.cinquecento.sporteventmanagementsystem.util.validators.ParticipantValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/participant")
 public class ParticipantController {
 
-    @GetMapping("/login")
-    public String login() {return "participants/participantLogin";}
+    private final ParticipantsService participantsService;
+    private final ParticipantValidator participantValidator;
 
+    @Autowired
+    public ParticipantController(ParticipantsService participantsService, ParticipantValidator participantValidator) {
+        this.participantsService = participantsService;
+        this.participantValidator = participantValidator;
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().
+                getAuthentication();
+        ParticipantsDetails participantsDetails = (ParticipantsDetails) authentication.getPrincipal();
+        Participant participant = participantsDetails.getParticipant();
+        model.addAttribute("participant", participant);
+
+        return "participant/info";
+    }
+
+    @GetMapping("/results")
+    public String results() {
+        return "participant/results";
+    }
 }
