bbb92d5e3ad5cc4ba2627fe0adbc67632b4696f6
cinquecentoandrey <chehonte16@gmail.com>
2022-09-11 13:06:36 +0300

    EventsController.java update

--- src/main/java/com/cinquecento/sporteventmanagementsystem/controller/EventsController.java
+++ src/main/java/com/cinquecento/sporteventmanagementsystem/controller/EventsController.java
@@ -1,13 +1,29 @@
 package com.cinquecento.sporteventmanagementsystem.controller;
 
+import com.cinquecento.sporteventmanagementsystem.service.EventsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/events")
 public class EventsController {
 
+    private final EventsService eventsService;
+
+    @Autowired
+    public EventsController(EventsService eventsService) {
+        this.eventsService = eventsService;
+    }
+
     @GetMapping()
-    public String index() {return "events/index";}
+    public String index() {return "event/index";}
+
+    @GetMapping("/show")
+    public void show() {
+        eventsService.findAll();
+    }
+
 }
