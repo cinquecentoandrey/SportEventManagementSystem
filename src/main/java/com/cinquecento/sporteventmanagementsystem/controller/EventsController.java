a55c3bd104dcc9c5bbe88976aa3ef5f3ae7b26d7
cinquecentoandrey <chehonte16@gmail.com>
2022-09-27 18:42:24 +0300

    Add the ability to register for an event

--- src/main/java/com/cinquecento/sporteventmanagementsystem/controller/EventsController.java
+++ src/main/java/com/cinquecento/sporteventmanagementsystem/controller/EventsController.java
@@ -1,10 +1,20 @@
 package com.cinquecento.sporteventmanagementsystem.controller;
 
+import com.cinquecento.sporteventmanagementsystem.model.Event;
+import com.cinquecento.sporteventmanagementsystem.model.Participant;
+import com.cinquecento.sporteventmanagementsystem.security.ParticipantsDetails;
 import com.cinquecento.sporteventmanagementsystem.service.EventsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -21,9 +31,31 @@ public class EventsController {
     @GetMapping()
     public String index() {return "event/index";}
 
-    @GetMapping("/show")
-    public void show() {
-        eventsService.findAll();
+    @GetMapping("/showAll")
+    public String showAll(Model model) {
+        List<Event> events = eventsService.findAll();
+        model.addAttribute("events", events);
+        return "event/index";
     }
 
+    @PostMapping("/registrationOnEvent/{id}")
+    public String registrationOnEvent(@PathVariable("id") int id,
+                                      Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().
+                getAuthentication();
+        ParticipantsDetails participantsDetails = (ParticipantsDetails) authentication.getPrincipal();
+        Participant participant = participantsDetails.getParticipant();
+
+        eventsService.addParticipant(id, participant);
+
+        model.addAttribute("participant", participant);
+        return "participant/info";
+    }
+
+    @GetMapping("/{id}")
+    public String showById(@PathVariable("id") int id,
+                           Model model) {
+        model.addAttribute("event", eventsService.findById(id).get());
+        return "event/show";
+    }
 }
